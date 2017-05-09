da44037616ad50cd835d31af1cacad4101caf9ed
Dmitry Dushkin <dndushkin@yandex-team.ru>
2017-05-09 11:54:49 +0300

    Fix home bin and add fish prompt

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -3,8 +3,7 @@
 # automatically. The node modules binaries are also handy to have around. I
 # don't like installing global node packages.
 # sbin is for brew
-set -gx PATH $PATH "~/bin" "./bin" "./node_modules/.bin"
-set -gx PATH $PATH "~/bin" "./bin" "./node_modules/.bin"
+set -gx PATH $PATH "$HOME/bin" "./node_modules/.bin"
 set -gx JAVA_HOME (/usr/libexec/java_home)
 
 # Global node_modules is bad idea! When needed use "npm link browser-sync" for example
@@ -56,4 +55,73 @@ function unlink_node_modules
   set node_modules_link (readlink node_modules)
   unlink node_modules
   mv $node_modules_link node_modules
+end
+
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+  set -l normal (set_color normal)
+
+  # Hack; fish_config only copies the fish_prompt function (see #736)
+  if not set -q -g __fish_classic_git_functions_defined
+    set -g __fish_classic_git_functions_defined
+
+    function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+      if status --is-interactive
+        commandline -f repaint ^/dev/null
+      end
+    end
+
+    function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+      if status --is-interactive
+        commandline -f repaint ^/dev/null
+      end
+    end
+
+    function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+      if status --is-interactive
+        commandline -f repaint ^/dev/null
+      end
+    end
+
+    function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
+      if status --is-interactive
+        commandline -f repaint ^/dev/null
+      end
+    end
+
+    # initialize our new variables
+    if not set -q __fish_classic_git_prompt_initialized
+      set -qU fish_color_user
+      or set -U fish_color_user -o green
+      set -qU fish_color_host
+      or set -U fish_color_host -o cyan
+      set -qU fish_color_status
+      or set -U fish_color_status red
+      set -U __fish_classic_git_prompt_initialized
+    end
+  end
+
+  set -l color_cwd
+  set -l prefix
+  switch $USER
+    case root toor
+      if set -q fish_color_cwd_root
+        set color_cwd $fish_color_cwd_root
+      else
+        set color_cwd $fish_color_cwd
+      end
+      set suffix '#'
+    case '*'
+      set color_cwd $fish_color_cwd
+      set suffix '>'
+  end
+
+  set -l prompt_status
+  if test $last_status -ne 0
+    set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
+  end
+
+  echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
+
+  # echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
 end
